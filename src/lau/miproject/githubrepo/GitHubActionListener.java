06fd3835e792ab58cac6e96086554d51e456e018
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-04-20 10:11:48 +0200

    Crear repositorio remoto

--- src/lau/miproject/githubrepo/GitHubActionListener.java
+++ src/lau/miproject/githubrepo/GitHubActionListener.java
@@ -2,9 +2,13 @@ package lau.miproject.githubrepo;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import org.kohsuke.github.GHCreateRepositoryBuilder;
+import org.kohsuke.github.GitHub;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 
 @ActionID(
@@ -21,6 +25,14 @@ public final class GitHubActionListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        GitHub github;
+        try {
+            github = GitHub.connect();
+            GHCreateRepositoryBuilder builder;
+            builder = github.createRepository("reepo");
+            builder.create();
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 }
